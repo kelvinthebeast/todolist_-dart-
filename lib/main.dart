de0552f967ae08195b36bcbcf109dde8b5f11619
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2024-11-22 22:21:05 +0700

    add modal and add task button

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,29 @@ class MyApp extends StatelessWidget {
           ],
         )
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {},
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showModalBottomSheet(context: context, builder: (BuildContext content) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.amber,
+            child: Column(
+              children:  [TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder()
+                ),
+              ),
+              SizedBox(height: 20,),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(onPressed: () {}, child: Text("Add task")))
+
+            ],),
+          );
+        });
+      },
       child: const Icon(Icons.add, size: 40),
       backgroundColor: Colors.blue[600],
       
